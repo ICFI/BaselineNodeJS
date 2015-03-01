41c291252bde2ebd9832cb0b21e26aff9b8ccf3f
Andrew Zeswitz <azeswitz@gmail.com>
2015-03-01 18:05:09 +0000

    modify host and port specification

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('host', process.env.HOST || '0.0.0.0');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-process.env.PORT, process.env.IP
 
-var server = app.listen(process.env.PORT, process.env.IP, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-})
+app.listen(app.get('port'), app.get('host'), function () {
+ 
+ console.log("Express server listening on port " + app.get('port'));
+ 
+});
