953af208fd9408c93f5cc0e26feb24a17e2ed41e
Andrew Zeswitz <azeswitz@gmail.com>
2015-03-04 16:37:02 +0000

    Refactor static file locations; add .bowerrc file for configuration

--- server.js
+++ server.js
@@ -2,17 +2,29 @@ var express = require('express');
 var session = require('express-session');
 var helmet = require('helmet');
 var csrf = require('csurf');
+var stylus = require('stylus');
 
 var shutting_down = false;
 var server = null;
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
-app.disable("x-powered-by");
+app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+}));
 
+app.use(express.static(__dirname + '/public'));
+
+app.disable("x-powered-by");
 app.use(session({
     secret: "Kuj6Gf",
     key: "sessionId",
