3006ef52f9703dc39d315f59f2aa95b23ae4c691
Andrew Zeswitz <azeswitz@gmail.com>
2015-03-04 13:38:04 +0000

    Add timestamp to output string

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.set('port', process.env.PORT || 3000);
 app.set('host', process.env.HOST || '0.0.0.0');
 
 app.get('/', function (req, res) {
-  res.send('Good morning ICF!');
+    var curTime = new Date().getTime();
+  res.send('Good morning ICF! ' + curTime);
 });
 
 
