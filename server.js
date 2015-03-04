a38e9232adce8d9a8f9220dc79629b3aaa43920e
Andrew Zeswitz <azeswitz@gmail.com>
2015-03-04 12:20:53 +0000

    Make text change - test deployment watching with pm2

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('host', process.env.HOST || '0.0.0.0');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Good morning ICF!');
 });
 
 
